dba788fa86869380eea4c757b762c93df1cfeeab
gavintan1234 <gttan@ucsd.edu>
2022-10-14 18:36:11 -0700

    fix filter method bug

--- ListTests.java
+++ ListTests.java
@@ -1,9 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListTests {
 
-    class LongStringChecker implements StringChecker {
+    public class LongStringChecker implements StringChecker {
         @Override
         public boolean checkString(String s) {
             if (s.length() > 5) {
@@ -12,6 +14,23 @@ public class ListTests {
                 return false;
             }
         }
+    }
+
+    @Test
+    public void testFilter() {
+        ArrayList<String> input = new ArrayList<>();
+        input.add("cat");
+        input.add("dog");
+        input.add("monkey");
+        input.add("bird");
+        input.add("tiger");
+        input.add("giraffe");
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("monkey");
+        expected.add("giraffe");
+        LongStringChecker lsc = new LongStringChecker();
+        ArrayList<String> output = (ArrayList<String>) ListExamples.filter(input, lsc);
+        assertEquals(expected, output);
     }
 }
 
